828b6b15b566a4a471a1e9f5d32533e7e672dc72
NVTFT <65675853+nguyenvantien0903@users.noreply.github.com>
2022-06-07 19:28:31 +0700

    create blog

--- src/main/java/com/SE1614/Group6/Repo/UserRepository.java
+++ src/main/java/com/SE1614/Group6/Repo/UserRepository.java
@@ -2,7 +2,9 @@ package com.SE1614.Group6.Repo;
 
 import com.SE1614.Group6.Model.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     public Long countById(Integer id);
 }
