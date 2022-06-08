894e97e8b71e64fb63230e1efa6897dc0798950a
hoacmhe150337 <hoacmhe150337@fpt.edu.vn>
2022-06-08 09:36:16 +0700

    tạo product

--- src/main/java/com/SE1614/Group6/Controller/ProductController.java
+++ src/main/java/com/SE1614/Group6/Controller/ProductController.java
@@ -1,4 +1,45 @@
 package com.SE1614.Group6.Controller;
 
+import com.SE1614.Group6.Model.Feedback;
+import com.SE1614.Group6.Model.Product;
+import com.SE1614.Group6.Repo.ProductRepository;
+import com.SE1614.Group6.Service.FeedbackService;
+import com.SE1614.Group6.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+
+
+@Controller
 public class ProductController {
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/product")
+    public String showProductList(Model model){
+        List<Product> listProduct = service.listAllProduct();
+        model.addAttribute("listProduct",listProduct);
+        return "product";
+    }
+
+    @GetMapping("/product/new")
+    public String showNewForm(Model model){
+        model.addAttribute("product",new Product());
+        model.addAttribute("pageTitle","Add New Product");
+        return "product_form";
+    }
+
+    @PostMapping("/product/save")
+    public String saveProduct(Product product){
+        service.save(product);
+
+        return "redirect:/product";
+    }
+
 }
