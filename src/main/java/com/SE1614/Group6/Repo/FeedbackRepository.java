397d263362a693128269026f15dd1e244202145e
khoatndhe153511 <khoatndhe153511@fpt.edu.vn>
2022-06-11 23:37:34 +0700

    FeedbackRepository

--- src/main/java/com/SE1614/Group6/Repo/FeedbackRepository.java
+++ src/main/java/com/SE1614/Group6/Repo/FeedbackRepository.java
@@ -4,5 +4,12 @@ import com.SE1614.Group6.Model.Feedback;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FeedbackRepository extends CrudRepository<Feedback, Integer>  {
+    @Override
+    Iterable<Feedback> findAll();
 
+    @Override
+    <S extends Feedback> S save(S entity);
+
+    @Override
+    <S extends Feedback> Iterable<S> saveAll(Iterable<S> entities);
 }
